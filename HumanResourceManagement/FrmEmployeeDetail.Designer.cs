bac1b021f2095ed8f512f91518dc8811c8cd70ce
VieLongT <vielongt52@gmail.com>
2021-10-12 22:08:38 +0700

    Add project files.

---
+++ HumanResourceManagement/FrmEmployeeDetail.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace HumanResourceManagement
+{
+    partial class FrmEmployeeDetail
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mtxtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboDegree = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvEmployee = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbTitle.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbTitle.Location = new System.Drawing.Point(378, 22);
+            this.lbTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(233, 40);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Employee Detail";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(492, 106);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employee ID";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEmployeeID.Location = new System.Drawing.Point(607, 104);
+            this.txtEmployeeID.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(261, 27);
+            this.txtEmployeeID.TabIndex = 2;
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEmployeeName.Location = new System.Drawing.Point(607, 152);
+            this.txtEmployeeName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(261, 27);
+            this.txtEmployeeName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(492, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Employee Name";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(492, 209);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Phone";
+            // 
+            // mtxtPhone
+            // 
+            this.mtxtPhone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.mtxtPhone.Location = new System.Drawing.Point(607, 207);
+            this.mtxtPhone.Margin = new System.Windows.Forms.Padding(2);
+            this.mtxtPhone.Mask = "000-0000000";
+            this.mtxtPhone.Name = "mtxtPhone";
+            this.mtxtPhone.Size = new System.Drawing.Size(261, 27);
+            this.mtxtPhone.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(492, 268);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Gender";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.rbFemale);
+            this.groupBox1.Controls.Add(this.rbMale);
+            this.groupBox1.Location = new System.Drawing.Point(607, 246);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(260, 60);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(146, 24);
+            this.rbFemale.Margin = new System.Windows.Forms.Padding(2);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(78, 24);
+            this.rbFemale.TabIndex = 1;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Checked = true;
+            this.rbMale.Location = new System.Drawing.Point(14, 24);
+            this.rbMale.Margin = new System.Windows.Forms.Padding(2);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(63, 24);
+            this.rbMale.TabIndex = 0;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(492, 327);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Degree";
+            // 
+            // cboDegree
+            // 
+            this.cboDegree.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboDegree.FormattingEnabled = true;
+            this.cboDegree.Items.AddRange(new object[] {
+            "Ph. D.",
+            "Master",
+            "Engineer",
+            "Bachelor",
+            "Technician"});
+            this.cboDegree.Location = new System.Drawing.Point(607, 327);
+            this.cboDegree.Margin = new System.Windows.Forms.Padding(2);
+            this.cboDegree.Name = "cboDegree";
+            this.cboDegree.Size = new System.Drawing.Size(261, 28);
+            this.cboDegree.TabIndex = 10;
+            this.cboDegree.Text = "-- Select Degree --";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(561, 389);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(108, 29);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(696, 389);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(108, 29);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // dgvEmployee
+            // 
+            this.dgvEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployee.Location = new System.Drawing.Point(25, 104);
+            this.dgvEmployee.Name = "dgvEmployee";
+            this.dgvEmployee.RowHeadersWidth = 51;
+            this.dgvEmployee.RowTemplate.Height = 29;
+            this.dgvEmployee.Size = new System.Drawing.Size(423, 314);
+            this.dgvEmployee.TabIndex = 13;
+            this.dgvEmployee.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmployee_CellDoubleClick);
+            // 
+            // FrmEmployeeDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(909, 448);
+            this.Controls.Add(this.dgvEmployee);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cboDegree);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mtxtPhone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FrmEmployeeDetail";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form add new Employee";
+            this.Load += new System.EventHandler(this.FrmEmployeeDetail_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox mtxtPhone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboDegree;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dgvEmployee;
+    }
+}
+
---
+++ HumanResourceManagement/FrmEmployeeDetail.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumanResourceManagement
+{
+    public partial class FrmEmployeeDetail : Form
+    {
+        public FrmEmployeeDetail()
+        {
+            InitializeComponent();
+        }
+
+        void ReloadForm()
+        {
+            dgvEmployee.DataSource = null;
+            dgvEmployee.DataSource = list;
+            dgvEmployee.Columns[0].HeaderText = "Employee ID";
+            dgvEmployee.Columns[0].ReadOnly = true;
+            dgvEmployee.Columns[1].HeaderText = "Name";
+            dgvEmployee.Columns[1].ReadOnly = true;
+            dgvEmployee.Columns[2].HeaderText = "Phone";
+            dgvEmployee.Columns[2].ReadOnly = true;
+            dgvEmployee.Columns[3].HeaderText = "Gender";
+            dgvEmployee.Columns[3].ReadOnly = true;
+            dgvEmployee.Columns[4].HeaderText = "Degree";
+            dgvEmployee.Columns[4].ReadOnly = true;
+        }
+
+        Employee checkExist(Employee emp)
+        {
+            foreach(var e in list)
+            {
+                if (e.EmployeeID.Equals(emp.EmployeeID)) return e;
+            }
+            return null;
+        }
+
+        bool ValidForm()
+        {
+            bool flag = true;
+            string strError = "";
+
+            string empID = txtEmployeeID.Text.Trim();
+            string empName = txtEmployeeName.Text.Trim();
+            string degree = cboDegree.SelectedIndex != -1 ? cboDegree.SelectedItem.ToString() : "";
+
+            Regex regEmpId = new Regex(@"^E\d{4}$");
+            if (!regEmpId.IsMatch(empID))
+            {
+                flag = false;
+                strError += "Employee ID is invalid\n";
+                txtEmployeeID.Focus();
+            }
+
+            Regex regEmpName = new Regex(@"^[\w\s]+$");
+            if (!regEmpName.IsMatch(empName))
+            {
+                flag = false;
+                strError += "Employee Name is invalid\n";
+                txtEmployeeID.Focus();
+            }
+
+            if(degree == "")
+            {
+                flag = false;
+                strError += "Degree must be selected";
+            }
+
+            if(!flag) MessageBox.Show(strError);
+            return flag;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // neu du lieu tren form la hop le thi hien thi du lieu tren form, neu khong thi
+            if (ValidForm())
+            {
+                //string str = "";
+                //str += "Emp ID: " + txtEmployeeID.Text.Trim() + "\n";
+                //str += "Emp name: " + txtEmployeeName.Text.Trim() + "\n";
+                //str += "Phone: " + mtxtPhone.Text.Trim() + "\n";
+                //str += "Gender: " + (rbMale.Checked ? "Male" : "Female") +"\n";
+                //str += "Degree: " + cboDegree.SelectedItem.ToString();
+                //MessageBox.Show(str);
+
+                Employee emp = new Employee() { EmployeeID = txtEmployeeID.Text.Trim(), 
+                                                EmployeeName = txtEmployeeName.Text.Trim(), 
+                                                Phone = mtxtPhone.Text.Trim(), 
+                                                Gender = (rbMale.Checked ? "Male" : "Female"), 
+                                                Degree = cboDegree.SelectedItem.ToString()};
+                
+                if (checkExist(emp) == null)
+                {
+                    list.Add(emp);
+                    txtEmployeeID.Clear();
+                    txtEmployeeName.Clear();
+                    mtxtPhone.Clear();
+                    rbMale.Checked = true;
+                    rbFemale.Checked = false;
+                    cboDegree.ResetText();
+                }
+                else
+                {
+                    Employee updateEmp = checkExist(emp);
+                    updateEmp.EmployeeName = txtEmployeeName.Text.Trim();
+                    updateEmp.Phone = mtxtPhone.Text.Trim();
+                    updateEmp.Gender = rbMale.Checked ? "Male" : "Female";
+                    updateEmp.Degree = cboDegree.SelectedItem.ToString();
+                    txtEmployeeID.Clear();
+                    txtEmployeeName.Clear();
+                    mtxtPhone.Clear();
+                    rbMale.Checked = true;
+                    rbFemale.Checked = false;
+                    cboDegree.ResetText();
+                    btnSave.Text = "Save";
+                }
+                ReloadForm();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            // neu muon thoat het cac cua so kha, Application.Exit()
+        }
+
+        List<Employee> list = new List<Employee>();
+
+        private void FrmEmployeeDetail_Load(object sender, EventArgs e)
+        {
+            list.Add(new Employee() { EmployeeID = "E0001", EmployeeName = "Nguyen Long", Phone = "096-3487529", Gender = "Male", Degree = "Engineer" });
+            ReloadForm();
+        }
+
+        private void dgvEmployee_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtEmployeeID.Text = this.dgvEmployee.CurrentRow.Cells[0].Value.ToString();
+            txtEmployeeName.Text = this.dgvEmployee.CurrentRow.Cells[1].Value.ToString();
+            mtxtPhone.Text = this.dgvEmployee.CurrentRow.Cells[2].Value.ToString();
+            String gender = this.dgvEmployee.CurrentRow.Cells[3].Value.ToString();
+            if (gender == "Male") rbMale.Checked = true;
+            else rbFemale.Checked = true;
+            cboDegree.SelectedItem = this.dgvEmployee.CurrentRow.Cells[4].Value.ToString();
+            btnSave.Text = "Update";
+        }
+    }
+
+    class Employee
+    {
+        public string EmployeeID { get; set; }
+
+        public string EmployeeName { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public string Degree { get; set; }
+    }
+
+}
